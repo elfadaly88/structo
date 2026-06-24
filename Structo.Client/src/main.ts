05ae209b2a6c32ffb25468978dfd50793e386230
mostafa <mostafa.sayed@miuegypt.edu.eg>
2026-06-24 13:23:06 +0300

    Merge remote changes and fix UI folder tracking222

--- Structo.Client/src/main.ts
+++ Structo.Client/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { environment } from './environments/environment';
 
+console.log('%c🚀 Structo App Environment Config:', 'color: #00ff00; font-weight: bold; font-size: 14px;');
+console.log('Is Production:', environment.production);
+console.log('API URL Absolute:', (environment as any).apiUrl);
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
