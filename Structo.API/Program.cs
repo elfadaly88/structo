bce544aa3221134e52517a2fefe42a245eb29ae4
mostafa <mostafa.sayed@miuegypt.edu.eg>
2026-06-30 13:30:14 +0300

    fix: implement CustomAwsHttpClientFactory and resolve CS0144/CS1061 compilation errors

--- Structo.API/Program.cs
+++ Structo.API/Program.cs
@@ -142,17 +142,15 @@ builder.Services.Configure<Structo.Core.Settings.CloudflareR2Settings>(builder.C
 builder.Services.AddSingleton<Amazon.S3.IAmazonS3>(sp =>
 {
     var settings = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<Structo.Core.Settings.CloudflareR2Settings>>().Value;
-    
-    // 👈 التكة القاضية: شيل الـ https وخلي الـ Endpoint يقرأ المسار من غير تعقيد SSL
-    var serviceUrl = builder.Configuration["CloudflareR2:ServiceUrl"]
-                        ?.Replace("https://", "http://") ?? "http://517b868184374750bced4ca6891f3a93.r2.cloudflarestorage.com";
+    var serviceUrl = builder.Configuration["CloudflareR2:ServiceUrl"]?.Replace("http://", "https://");
     
     var config = new Amazon.S3.AmazonS3Config
     {
         ServiceURL = serviceUrl,
-        UseHttp = true, // 👈 اجبرها على true لتخطي لغم الـ OpenSSL Handshake تماماً
+        UseHttp = false,
         ForcePathStyle = true,
-        AuthenticationRegion = "auto"
+        AuthenticationRegion = "auto",
+        HttpClientFactory = new CustomAwsHttpClientFactory()
     };
 
     var credentials = new Amazon.Runtime.BasicAWSCredentials(settings.AccessKeyId, settings.SecretAccessKey);
@@ -348,7 +346,7 @@ namespace Structo.API
     } 
 }
 
-public class CustomHttpClientFactory : Amazon.Runtime.HttpClientFactory
+public class CustomAwsHttpClientFactory : Amazon.Runtime.HttpClientFactory
 {
     public override System.Net.Http.HttpClient CreateHttpClient(Amazon.Runtime.IClientConfig clientConfig)
     {
