b64ecd7f47a10487b403d4dcfb228ef0521d7bd1
mostafa <mostafa.sayed@miuegypt.edu.eg>
2026-07-05 00:43:08 +0300

    chore: generate build artifacts for Structo.API project

--- Structo.API/obj/Debug/net9.0/Structo.API.AssemblyInfo.cs
+++ Structo.API/obj/Debug/net9.0/Structo.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Structo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ce849d5a6dbe2b9dfc736ff0dd614a16353ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1bd43436e2cb258a4beeebb72785f672595950d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Structo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Structo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
