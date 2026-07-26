f9b698906b9c402df83bcf64c571b799be83829b
mostafa <mostafa.sayed@miuegypt.edu.eg>
2026-07-26 15:12:18 +0300

    feat(mobile): configure Capacitor & add command-line Android APK build script (npm run apk:build)

---
+++ Structo.Client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.structo.app',
+  appName: 'Structo',
+  webDir: '../Structo.API/wwwroot/browser'
+};
+
+export default config;
