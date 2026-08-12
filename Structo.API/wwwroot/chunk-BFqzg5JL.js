3160cca603606c948fbd5818da6d4881bf928c8b
mostafa <mostafa.sayed@miuegypt.edu.eg>
2026-08-12 19:44:43 +0300

    fix(env): strictly enforce Railway production API URL in environment.prod.ts and wwwroot production bundles

---
+++ Structo.API/wwwroot/chunk-BFqzg5JL.js
@@ -0,0 +1,1 @@
+import {D,at as et,aJ as fh,aA as N}from'./main-CMK2GRHJ.js';function w(t){t||(t=D(et));let i=new N(e=>{if(t.destroyed){e.next();return}return t.onDestroy(e.next.bind(e))});return e=>e.pipe(fh(i))}export{w};
